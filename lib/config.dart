bab956386e60c5a177214914eaa3fc4882a6f9e7
bao.ta <tathanhbaobk@gmail.com>
2023-03-31 17:34:05 +0700

    hehe

---
+++ lib/config.dart
@@ -0,0 +1,11 @@
+
+import 'package:camera_deep_ar/camera_deep_ar.dart';
+
+DeepArConfig config = DeepArConfig(
+  androidKey:
+  "b403972e2eddc8ae95152dc1c0512936dee15654353b0a2481d35ed8561afe730daef9450f3a260d",
+  ioskey:
+  "b403972e2eddc8ae95152dc1c0512936dee15654353b0a2481d35ed8561afe730daef9450f3a260d",
+  displayMode: DisplayMode.camera,
+// displayMode: DisplayMode.camera,
+);
